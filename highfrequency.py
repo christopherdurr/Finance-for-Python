a0879467e306a347df89212f329e43b45a058c6e
Christopher Durr <cdurr@uci.edu>
2017-02-21 09:44:12 -0800

    Update highfrequency.py

--- highfrequency.py
+++ highfrequency.py
@@ -14,7 +14,7 @@ days = ['22', '23', '24', '25']
 AAPL = pd.DataFrame()
 for day in days:
   AAPL = AAPL.append(pd.read_csv(url % (year, month, day), index_col = 9, header = 0, parse_dates = True))
-AAPL.columns = ['bid', 'bdepth', 'bdeptht', offer', 'odepth', 'odeptht']
+AAPL.columns = ['bid', 'bdepth', 'bdeptht', 'offer', 'odepth', 'odeptht']
 AAPL.info()
 
 AAPL['bid'].plot()
